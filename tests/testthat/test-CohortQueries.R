dbbfd26979e8eefe2af976355f39a96ba42e27ad
jreps <jreps@its.jnj.com>
2025-02-14 08:37:43 -0500

    Update test-CohortQueries.R
    - adding more tests to cover cohort generator results

--- tests/testthat/test-CohortQueries.R
+++ tests/testthat/test-CohortQueries.R
@@ -19,3 +19,41 @@ test_that("getCohortDefinitions", {
   testthat::expect_true('subsetDefinitionId' %in% colnames(cohorts))
   
 })
+
+test_that("processCohorts", {
+  
+  cohorts <- getCohortDefinitions(
+    connectionHandler = connectionHandler,
+    schema = schema
+  )
+
+  parents <- processCohorts(cohorts)
+  
+  # check correct number of parents
+  testthat::expect_true(sum(is.na(cohorts$subsetDefinitionId)) == length(parents$parents))
+  
+  # check all cohorts are in the parents list
+  testthat::expect_true(nrow(do.call('rbind', parents$cohortList)) == nrow(cohorts))
+  
+})
+
+
+test_that("getCohortSubsetDefinitions", {
+  
+  subsetDef <- getCohortSubsetDefinitions(
+    connectionHandler,
+    schema
+    )
+  
+  # make sure results are returned
+  testthat::expect_true(nrow(subsetDef) > 0)
+  
+  firstJson <- ParallelLogger::convertJsonToSettings(subsetDef$json[1])
+  
+  # check key entries
+  testthat::expect_true('name' %in% names(firstJson))
+  testthat::expect_true('definitionId' %in% names(firstJson))
+  testthat::expect_true('subsetOperators' %in% names(firstJson))
+  
+})
+  
