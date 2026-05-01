a7dd8ce239c6248ee011c38a03a97ff537c4f298
jreps <jreps@its.jnj.com>
2026-05-01 11:48:08 -0400

    plot updates
    - updating Incidence rate table
    - updating CM/SCCS plots
    - removing presentation template as report template covers it
    - minor updates the SCCS extraction to make it backwards compatible for plots

--- inst/sql/sql_server/estimation/getCmMetaEstimationV5.sql
+++ inst/sql/sql_server/estimation/getCmMetaEstimationV5.sql
@@ -24,6 +24,12 @@ SELECT ev.evidence_synthesis_description AS database_name
 	,r.comparator_outcomes
 	,unblind.unblind
 	,r.n_databases
+	,NULL as pi_95_lb
+	,NULL as r.pi_95_ub
+	,NULL as calibrated_pi_95_lb
+	,NULL as calibrated_pi_95_ub
+	,unblind.tau
+	,unblind.i_2
 FROM @schema.@es_table_prefixcm_result AS r
 INNER JOIN @schema.@cm_table_prefixtarget_comparator_outcome AS tco ON r.target_id = tco.target_id
 	AND r.comparator_id = tco.comparator_id
