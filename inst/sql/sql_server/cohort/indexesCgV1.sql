f30c111efc8de555ebcb78aa89a9dff5ea0e3669
jreps <jreps@its.jnj.com>
2026-05-07 14:19:18 -0400

    improving target fetching speed
    - restructured RF and CS target extraction SQL to use indexes
    - added indexes for cohort generator templates

--- inst/sql/sql_server/cohort/indexesCgV1.sql
+++ inst/sql/sql_server/cohort/indexesCgV1.sql
@@ -4,3 +4,9 @@ ANALYZE @schema.@cg_table_prefixcohort_count;
 CREATE INDEX IF NOT EXISTS cg_cd_subset ON @schema.@cg_table_prefixcohort_definition(subset_definition_id);
 CREATE INDEX IF NOT EXISTS cg_cd_id ON @schema.@cg_table_prefixcohort_definition(cohort_definition_id);
 ANALYZE @schema.@cg_table_prefixcohort_definition;
+
+CREATE INDEX IF NOT EXISTS cg_ctl_cdef_tdef ON @schema.@cg_table_prefixcohort_template_link(cohort_definition_id,template_definition_id);
+ANALYZE @schema.@cg_table_prefixcohort_template_link;
+
+CREATE INDEX IF NOT EXISTS cg_ctd_tdef ON @schema.@cg_table_prefixcohort_template_definition(template_definition_id);
+ANALYZE @schema.@cg_table_prefixcohort_template_definition;
